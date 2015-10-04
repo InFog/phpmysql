cbeed31103ebb34a01f7e188f83f822373ebabdd
Evaldo Junior <evaldojuniorbento@gmail.com>
2015-10-04 13:35:30 +0200

    Atualizacoes no exemplo dos emails.

--- edicao2/afazeresemail/editar.php
+++ edicao2/afazeresemail/editar.php
@@ -15,20 +15,20 @@ if (tem_post()) {
 
     $tarefa['id'] = $_POST['id'];
 
-    if (isset($_POST['nome']) && strlen($_POST['nome']) > 0) {
+    if (array_key_exists('nome', $_POST) && $_POST['nome'] != '') {
         $tarefa['nome'] = $_POST['nome'];
     } else {
         $tem_erros = true;
         $erros_validacao['nome'] = 'O nome da tarefa é obrigatório!';
     }
 
-    if (isset($_POST['descricao'])) {
+    if (array_key_exists('descricao', $_POST)) {
         $tarefa['descricao'] = $_POST['descricao'];
     } else {
         $tarefa['descricao'] = '';
     }
 
-    if (isset($_POST['prazo']) && strlen($_POST['prazo']) > 0) {
+    if (array_key_exists('prazo', $_POST) && strlen($_POST['prazo']) > 0) {
         if (validar_data($_POST['prazo'])) {
             $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);
         } else {
@@ -41,7 +41,7 @@ if (tem_post()) {
 
     $tarefa['prioridade'] = $_POST['prioridade'];
 
-    if (isset($_POST['concluida'])) {
+    if (array_key_exists('concluida', $_POST)) {
         $tarefa['concluida'] = 1;
     } else {
         $tarefa['concluida'] = 0;
@@ -50,7 +50,7 @@ if (tem_post()) {
     if (! $tem_erros) {
         editar_tarefa($conexao, $tarefa);
 
-        if (isset($_POST['lembrete']) && $_POST['lembrete'] == '1') {
+        if (array_key_exists('lembrete', $_POST) && $_POST['lembrete'] == '1') {
             $anexos = buscar_anexos($conexao, $tarefa['id']);
 
             enviar_email($tarefa, $anexos);
--- edicao2/afazeresemail/tarefas.php
+++ edicao2/afazeresemail/tarefas.php
@@ -7,25 +7,25 @@ include "ajudantes.php";
 $exibir_tabela = true;
 
 $tem_erros = false;
-$erros_validacao = array();
+$erros_validacao = [];
 
 if (tem_post()) {
-    $tarefa = array();
+    $tarefa = [];
 
-    if (isset($_POST['nome']) && strlen($_POST['nome']) > 0) {
+    if (array_key_exists('nome', $_POST) && $_POST['nome'] != '') {
         $tarefa['nome'] = $_POST['nome'];
     } else {
         $tem_erros = true;
         $erros_validacao['nome'] = 'O nome da tarefa é obrigatório!';
     }
 
-    if (isset($_POST['descricao'])) {
+    if (array_key_exists('descricao', $_POST)) {
         $tarefa['descricao'] = $_POST['descricao'];
     } else {
         $tarefa['descricao'] = '';
     }
 
-    if (isset($_POST['prazo']) && strlen($_POST['prazo']) > 0) {
+    if (array_key_exists('prazo', $_POST) && strlen($_POST['prazo']) > 0) {
         if (validar_data($_POST['prazo'])) {
             $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);
         } else {
@@ -38,7 +38,7 @@ if (tem_post()) {
 
     $tarefa['prioridade'] = $_POST['prioridade'];
 
-    if (isset($_POST['concluida'])) {
+    if (array_key_exists('concluida', $_POST)) {
         $tarefa['concluida'] = 1;
     } else {
         $tarefa['concluida'] = 0;
@@ -47,7 +47,7 @@ if (tem_post()) {
     if (! $tem_erros) {
         gravar_tarefa($conexao, $tarefa);
 
-        if (isset($_POST['lembrete']) && $_POST['lembrete'] == '1') {
+        if (array_key_exists('lembrete', $_POST) && $_POST['lembrete'] == '1') {
             enviar_email($tarefa);
         }
 
@@ -58,13 +58,13 @@ if (tem_post()) {
 
 $lista_tarefas = buscar_tarefas($conexao);
 
-$tarefa = array(
+$tarefa = [
     'id'         => 0,
-    'nome'       => (isset($_POST['nome'])) ? $_POST['nome'] : '',
-    'descricao'  => (isset($_POST['descricao'])) ? $_POST['descricao'] : '',
-    'prazo'      => (isset($_POST['prazo'])) ? traduz_data_para_banco($_POST['prazo']) : '',
-    'prioridade' => (isset($_POST['prioridade'])) ? $_POST['prioridade'] : 1,
-    'concluida'  => (isset($_POST['concluida'])) ? $_POST['concluida'] : ''
-);
+    'nome'       => (array_key_exists('nome', $_POST)) ? $_POST['nome'] : '',
+    'descricao'  => (array_key_exists('descricao', $_POST)) ? $_POST['descricao'] : '',
+    'prazo'      => (array_key_exists('prazo', $_POST)) ? traduz_data_para_banco($_POST['prazo']) : '',
+    'prioridade' => (array_key_exists('prioridade', $_POST)) ? $_POST['prioridade'] : 1,
+    'concluida'  => (array_key_exists('concluida', $_POST)) ? $_POST['concluida'] : ''
+];
 
 include "template.php";
